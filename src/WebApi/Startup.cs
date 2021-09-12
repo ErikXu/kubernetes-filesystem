cd7930c1c7a0f065344ec88bdc91b9eac474fc3b
Erik Xu <81636759@qq.com>
2021-09-12 11:53:54 +0800

    Remove https.

--- src/WebApi/Startup.cs
+++ src/WebApi/Startup.cs
@@ -37,8 +37,6 @@ namespace WebApi
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi v1"));
             }
 
-            app.UseHttpsRedirection();
-
             app.UseRouting();
 
             app.UseAuthorization();
